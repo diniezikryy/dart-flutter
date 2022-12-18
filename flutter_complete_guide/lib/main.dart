1e9cb15c140cf7877a4f56a51468364f37dcc7e0
dinie zikry <diniezikry@dinies-MacBook-Pro.local>
2022-12-18 22:46:51 +0800

    2.39

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World!"),
+      ),
+      body: Column(children: <Widget>[
+        Question(questions[_questionIndex]),
+        ElevatedButton(
+          child: Text("Answer 1"),
+          onPressed: _answerQuestion,
+        ),
+        ElevatedButton(
+          child: Text("Answer 2"),
+          onPressed: (() => print("Answer 2 chosen!")),
+        ),
+        ElevatedButton(
+          child: Text("Answer 3"),
+          onPressed: _answerQuestion,
+        ),
+      ]),
+    ));
+  }
+}
