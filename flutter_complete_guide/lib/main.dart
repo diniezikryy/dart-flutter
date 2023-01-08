d60dd022c948bcec12735e759fdc7b3fe24acf56
dinie zikry <diniezikry@dinies-MacBook-Pro.local>
2023-01-09 02:07:25 +0800

    fixed flutter not showing output bug

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,19 +36,12 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Column(children: <Widget>[
         Question(questions[_questionIndex]),
-        ElevatedButton(
-          child: Text("Answer 1"),
-          onPressed: _answerQuestion,
-        ),
-        ElevatedButton(
-          child: Text("Answer 2"),
-          onPressed: (() => print("Answer 2 chosen!")),
-        ),
-        ElevatedButton(
-          child: Text("Answer 3"),
-          onPressed: _answerQuestion,
-        ),
+        Answer(_answerQuestion),
+        Answer(_answerQuestion),
+        Answer(_answerQuestion),
       ]),
     ));
   }
 }
+
+// Have one widget per file
