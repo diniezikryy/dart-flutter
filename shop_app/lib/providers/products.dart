46d7ea789c51e7409bacd23c9739d7a09a37aa9d
dinie zikry <dzrudi23@gmail.com>
2023-03-04 21:27:13 +0800

    can edit products within manage products screen

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -54,18 +54,31 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  // void showFavoritesOnly() {
-  //   _showFavoritesOnly = true;
-  //   notifyListeners();
-  // }
+  void addProduct(Product product) {
+    final newProduct = Product(
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      id: DateTime.now().toString(),
+    );
+    _items.add(newProduct);
+    notifyListeners();
+  }
 
-  // void showAll() {
-  //   _showFavoritesOnly = false;
-  //   notifyListeners();
-  // }
+  void updateProduct(String id, Product newProduct) {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      _items[prodIndex] = newProduct;
+      notifyListeners();
+    } else {
+      print('...');
+    }
+  }
 
-  void addProduct() {
-    // _items.add(value);
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+
     notifyListeners();
   }
 }
